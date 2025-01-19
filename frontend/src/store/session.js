5b9e44f66d70ede662c09224538307e8199f0502
OceanicPhoenix <runninggrandmaster@gmail.com>
2025-01-19 03:18:45 -0800

    frontend 3

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -48,7 +48,7 @@ export const restoreUser = () => async (dispatch) => {
     return response;
   };
 
-  
+
 //Signup
   export const signup = (user) => async (dispatch) => {
     const { username, firstName, lastName, email, password } = user;
@@ -63,7 +63,15 @@ export const restoreUser = () => async (dispatch) => {
       })
     });
     const data = await response.json();
-    dispatch(setUser(data.user));
+    dispatch(setSessionUser(data.user));
+    return response;
+  };
+
+  export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+      method: 'DELETE'
+    });
+    dispatch(removeUser());
     return response;
   };
 
